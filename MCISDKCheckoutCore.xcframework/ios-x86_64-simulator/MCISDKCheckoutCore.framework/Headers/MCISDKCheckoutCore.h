b7fac1102f2163a59561cf6af34477be846028fa
Rodrigo Brauwers <rodrigo.brauwers@dock.tech>
2022-09-26 08:35:23 -0300

    Adding frameworks

---
+++ MCISDKCheckoutCore.xcframework/ios-x86_64-simulator/MCISDKCheckoutCore.framework/Headers/MCISDKCheckoutCore.h
@@ -0,0 +1,19 @@
+//
+//  MCISDKCheckoutCore.h
+//  MCISDKCheckoutCore
+//
+//  Created by Denis Wilson de Souza Oliveira on 30/08/19.
+//  Copyright © 2019 Merci. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MCISDKCheckoutCore.
+FOUNDATION_EXPORT double MCISDKCheckoutCoreVersionNumber;
+
+//! Project version string for MCISDKCheckoutCore.
+FOUNDATION_EXPORT const unsigned char MCISDKCheckoutCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MCISDKCheckoutCore/PublicHeader.h>
+
+

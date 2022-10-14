74b6b64a29073a5d808eabe69fbcc3278e1510ce
Thais Sadami <thaissadami@gmail.com>
2022-10-14 11:58:54 -0300

    [Pod] - Version 2.0.0-alpha.12

---
+++ MCISDKAuth.xcframework/ios-arm64/MCISDKAuth.framework/Headers/MCISDKAuth.h
@@ -0,0 +1,19 @@
+//
+//  MCISDKAuth.h
+//  MCISDKAuth
+//
+//  Created by Denis Wilson de Souza Oliveira on 09/08/19.
+//  Copyright © 2019 Merci. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MCISDKAuth.
+FOUNDATION_EXPORT double MCISDKAuthVersionNumber;
+
+//! Project version string for MCISDKAuth.
+FOUNDATION_EXPORT const unsigned char MCISDKAuthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MCISDKAuth/PublicHeader.h>
+
+

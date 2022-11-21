53ff2009b08253734ef20e328bf0dbb790dc5019
Thais Sadami <thaissadami@gmail.com>
2022-11-21 18:41:12 -0300

    [Pod] - Version 1.9.12

--- MerciKit.xcframework/ios-arm64_i386_x86_64-simulator/MerciKit.framework/Headers/MerciKit-Swift.h
+++ MerciKit.xcframework/ios-arm64_i386_x86_64-simulator/MerciKit.framework/Headers/MerciKit-Swift.h
@@ -221,7 +221,6 @@ SWIFT_CLASS("_TtC8MerciKit21MCINotificationCenter")
 - (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
 @end
 
-
 #if __has_attribute(external_source_symbol)
 # pragma clang attribute pop
 #endif
@@ -450,7 +449,6 @@ SWIFT_CLASS("_TtC8MerciKit21MCINotificationCenter")
 - (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
 @end
 
-
 #if __has_attribute(external_source_symbol)
 # pragma clang attribute pop
 #endif
@@ -678,7 +676,6 @@ SWIFT_CLASS("_TtC8MerciKit21MCINotificationCenter")
 @interface MCINotificationCenter : NSObject
 - (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
 @end
-
 
 #if __has_attribute(external_source_symbol)
 # pragma clang attribute pop

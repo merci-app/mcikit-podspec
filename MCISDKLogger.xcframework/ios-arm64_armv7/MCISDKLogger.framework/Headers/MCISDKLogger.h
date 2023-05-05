49e8a132c8a59f2e6738de825aecdd227fc24c9a
runner <runner@Mac-1683311092544.local>
2023-05-05 20:44:26 +0000

    2.0.3: adding frameworks

---
+++ MCISDKLogger.xcframework/ios-arm64_armv7/MCISDKLogger.framework/Headers/MCISDKLogger.h
@@ -0,0 +1,19 @@
+//
+//  MCISDKLogger.h
+//  MCISDKLogger
+//
+//  Created by Thales Frigo on 12/08/19.
+//  Copyright © 2019 Merci. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MCISDKLogger.
+FOUNDATION_EXPORT double MCISDKLoggerVersionNumber;
+
+//! Project version string for MCISDKLogger.
+FOUNDATION_EXPORT const unsigned char MCISDKLoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MCISDKLogger/PublicHeader.h>
+
+

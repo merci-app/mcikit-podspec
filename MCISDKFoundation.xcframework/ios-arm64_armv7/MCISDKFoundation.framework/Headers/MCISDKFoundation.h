5b8dc3bb9ca0a3b18cfc9f676d7a72c66638a439
runner <runner@Mac-1683660565798.local>
2023-05-09 21:12:44 +0000

    2.0.4-RC.1: adding frameworks

---
+++ MCISDKFoundation.xcframework/ios-arm64_armv7/MCISDKFoundation.framework/Headers/MCISDKFoundation.h
@@ -0,0 +1,19 @@
+//
+//  MCISDKFoundation.h
+//  MCISDKFoundation
+//
+//  Created by Gustavo Cassab on 07/08/19.
+//  Copyright © 2019 Merci. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MCISDKFoundation.
+FOUNDATION_EXPORT double MCISDKFoundationVersionNumber;
+
+//! Project version string for MCISDKFoundation.
+FOUNDATION_EXPORT const unsigned char MCISDKFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MCISDKFoundation/PublicHeader.h>
+
+

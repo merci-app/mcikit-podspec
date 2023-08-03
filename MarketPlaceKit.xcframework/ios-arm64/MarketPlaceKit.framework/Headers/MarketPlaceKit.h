be6a4407a0428039ed4dafd8f3e7a451a10d77c5
runner <runner@Mac-1691082338502.local>
2023-08-03 18:09:42 +0000

    2.1.0: adding frameworks

---
+++ MarketPlaceKit.xcframework/ios-arm64/MarketPlaceKit.framework/Headers/MarketPlaceKit.h
@@ -0,0 +1,19 @@
+//
+//  MarketPlaceKit.h
+//  MarketPlaceKit
+//
+//  Created by Denis Wilson de Souza Oliveira on 02/04/20.
+//  Copyright © 2020 Merci. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MarketPlaceKit.
+FOUNDATION_EXPORT double MarketPlaceKitVersionNumber;
+
+//! Project version string for MarketPlaceKit.
+FOUNDATION_EXPORT const unsigned char MarketPlaceKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MarketPlaceKit/PublicHeader.h>
+
+

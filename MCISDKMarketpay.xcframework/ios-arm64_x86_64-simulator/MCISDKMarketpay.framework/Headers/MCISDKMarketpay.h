331e29e963bc3575bfbea1906e1f9f315ccf7c90
runner <runner@Mac-1719938714002.local>
2024-07-02 18:02:19 +0000

    2.2.5: adding frameworks

---
+++ MCISDKMarketpay.xcframework/ios-arm64_x86_64-simulator/MCISDKMarketpay.framework/Headers/MCISDKMarketpay.h
@@ -0,0 +1,19 @@
+//
+//  MCISDKMarketpay.h
+//  MCISDKMarketpay
+//
+//  Created by Gustavo Cassab on 07/08/19.
+//  Copyright © 2019 Merci. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MCISDKMarketpay.
+FOUNDATION_EXPORT double MCISDKMarketpayVersionNumber;
+
+//! Project version string for MCISDKMarketpay.
+FOUNDATION_EXPORT const unsigned char MCISDKMarketpayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MCISDKMarketpay/PublicHeader.h>
+
+
